7d3e13f34de1fe5d9692e63672a11b95aaf859f1
Dian Leithon Isidro <dlv.isidro@gmail.com>
2021-01-08 18:40:29 +0800

    clean up

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 require('dotenv').config();
 
 const express = require('express');
-const jwt = require('jsonwebtoken');
 const Database = require('./app/database');
 const app = express();
 const loyaltyApp = require('./app');
-const bcrypt = require('bcrypt');
 
 const database = Database.forUrl(process.env.db_url);
 loyaltyApp(database);
